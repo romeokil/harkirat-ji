cfed2a6e59f00c47af963c4ac426643a8b0ad836
Rahul Kumar <rahulkumarjha58978@gmail.com>
2024-11-19 02:01:26 +0530

    added backend of small doctor clinic wala but abhi maza ni aaega qki hm toh bs isme nos ke saath khel acutually me koi user ni bna rha hai.

--- week2/index.js
+++ week2/index.js
@@ -3,9 +3,73 @@ const express=require("express");
 const app=express();
 
 const PORT=3001;
-
+const users=[
+    {
+        name:"Rahul",
+        kidneys:[{
+            healthy:true,
+        },{
+            unhealthy:true
+        }]
+    }
+]
+// kidney game hmlog khelne wale hai.
 app.get('/',(req,res)=>{
-    res.send('<h1>hello baby</h1>')
+    let displaykidneys=users[0].kidneys;
+    let countkidneys=displaykidneys.length;
+    console.log("displaykidneys",displaykidneys);
+    console.log("countkidneys",countkidneys);
+    let healthykidneys=0,unhealthykidneys=0;
+    for(let i=0;i<displaykidneys.length;i++){
+        if(displaykidneys[i].healthy==true) healthykidneys++;
+        if(displaykidneys[i].unhealthy==true) unhealthykidneys++;
+    }
+    console.log("healthykidneys",healthykidneys);
+    console.log("unhealthykidneys",unhealthykidneys);
+    res.json({
+        countkidneys,
+        healthykidneys,
+        unhealthykidneys
+    })
+})
+app.post('/',(req,res)=>{
+    let kidneyswehavebeforeadd=users[0].kidneys.length;
+    let kidneycountafteraddition=kidneyswehavebeforeadd+1;
+    res.json({
+        kidneyswehavebeforeadd,
+        kidneycountafteraddition
+    })
+
+})
+app.put('/',(req,res)=>{
+    let unhealthykidneys=0;
+    let displaykidneys=users[0].kidneys;
+    for(let i=0;i<displaykidneys.length;i++){
+        if(displaykidneys[i].unhealthy==true) unhealthykidneys++;
+    }
+    console.log("unhealthy kidneys hai ",unhealthykidneys)
+    if(unhealthykidneys==0) res.status(411).json({
+        "msg":"bhai unhealthy kidney hai eich ni toh ky hi replace krega"
+    })
+    res.json({unhealthykidneys})
+})
+
+app.delete('/',(req,res)=>{
+    let displaykidneys=users[0].kidneys;
+    let unhealthykidneys=0;
+    for(let i=0;i<displaykidneys.length;i++){
+        if(displaykidneys[i].unhealthy==true) unhealthykidneys++;
+    }
+    if(unhealthykidneys==0) res.status(301).json({
+        "msg":"Bhai unhealthy kidney remove kaise krega hai hich ni re"
+    })
+    else{
+        users[0].kidneys.unhealthy=false;
+        res.status(201).json({
+            "msg":"Kidney is removed Successfully",
+            unhealthykidneys
+        })
+    }
 })
 
 app.listen(PORT,()=>{
