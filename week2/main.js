28327865d463f34b9e0709ec13ac85b171a792ca
Rahul Kumar <rahulkumarjha58978@gmail.com>
2024-11-21 16:13:37 +0530

    introduction zod lets check for array of numbers agr jyada pata krna hai toh documentation dekh skte ho.

--- week2/main.js
+++ week2/main.js
@@ -1,35 +1,23 @@
 const express=require("express");
-
+const zod=require("zod");
 const app=express();
 
+const schema=zod.array(zod.number());
 const PORT=8000;
 app.use(express.json())
-function usercheck(req,res,next){
-    const username=req.headers.username;
-    const password=req.headers.password;
-    console.log("username",username);
-    console.log("password ",password);
-    if(username!="rahul" || password!="pass"){
-        res.json({
-            "msg":"Username or password is wrong"
-        })
-    }
-    next();
-}
-function kidneycheck(req,res,next){
-    const kidneyid=req.query.kidneyid;
-    console.log("kidneyid ",kidneyid)
-    if((kidneyid!=1 && kidneyid!=2)){
-        res.json({
-            "msg":"Kidneyid is not appropriate!!!"
-        })
-    }
-    next();
-}
-app.get('/',usercheck,kidneycheck,(req,res)=>{
+app.get('/',(req,res)=>{
     console.log("First get route!!");
     res.json({
         "msg":"done!!!"
+    })
+})
+app.post('/checkkidneys',(req,res)=>{
+    const kidneys=req.body;
+    const kidneyslength=kidneys.length;
+    const checkkidneys=schema.safeParse(kidneys)
+    console.log(checkkidneys);
+    res.json({
+        checkkidneys
     })
 })
 
